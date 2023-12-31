7f1c831f7eb4f03a00e472b1e83574e5fe4cbd00
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2023-12-31 01:17:12 -0400

    Finalização JWT e adicionados testes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,68 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { describe } from 'node:test';
+import * as pactum from 'pactum';
+import { AppModule } from '../src/app.module';
+import { AuthDTO } from '../src/auth/dto';
+import { PrismaService } from '../src/prisma/prisma.service';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('Auth', () => {
+    describe('SignUp', () => {
+      it('Should SignUp', () => {
+        const dto: AuthDTO = {
+          email: 'test@test.com',
+          password: 'password-test',
+        };
+
+        return pactum
+          .spec()
+          .post('http://localhost:3333/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('SignIn', () => {
+      it.todo('Should SignIn');
+    });
+  });
+
+  describe('User', () => {
+    describe('Get User', () => {});
+    describe('Edit User', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create Bookmark', () => {});
+    describe('Get Bookmarks', () => {});
+    describe('Get Bookmark By Id', () => {});
+    describe('Edit Bookmark', () => {});
+    describe('Delete Bookmark', () => {});
   });
 });
